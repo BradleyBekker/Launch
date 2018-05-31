28503489ea782e712cfa9c9a2f0dcacde79826fd
JudaHensen <24311@ma-web.nl>
2018-05-31 15:39:51 +0200

    fixed rocket scripts

--- spacecake/Assets/Scripts/P2rocket.cs
+++ spacecake/Assets/Scripts/P2rocket.cs
@@ -7,10 +7,12 @@ public class P2rocket : MonoBehaviour {
     public bool part1 = false;
     public bool part2 = false;
     public bool part3 = false;
+    public bool part4 = false;
+    public bool part5 = false;
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (part1 && part2 && part3 && collision.gameObject.tag == "player2")
+        if (part1 && part2 && part3 && part4 && part5 && collision.gameObject.tag == "player2")
         {
             print("p2 win");
             SceneManager.LoadScene(3);
