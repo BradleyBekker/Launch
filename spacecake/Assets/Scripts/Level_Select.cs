1ad81980ac3409e0ac73bf1d1804f287e9a971f3
BradleyBekker <30977961+BradleyBekker@users.noreply.github.com>
2018-05-31 09:34:03 +0200

    finalized menu

--- spacecake/Assets/Scripts/Level_Select.cs
+++ spacecake/Assets/Scripts/Level_Select.cs
@@ -5,17 +5,41 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Level_Select : MonoBehaviour {
+    [SerializeField]
+    Image controlimage;
+    [SerializeField]
+    Image introimage;
+    float timer = 0;
 
-	// Use this for initialization
-	void Start () {
-        GameObject.Find("start_button").GetComponentInChildren<Text>().text = "START";
+    void Start () {
+        GameObject.Find("start_button").GetComponentInChildren<Text>().text = "";
     }
 	
   public void Onclick()
     {
-        SceneManager.UnloadScene("Menu");
-        print("scene unloaded");
-        SceneManager.LoadScene("ExpandingSpace");
+
+        introimage.enabled = true;
+
         
+    }
+    private void Update()
+    {
+        print(timer);
+        if (introimage.enabled || controlimage.enabled) {
+            timer += 1;
+            if(timer >= 225)
+            {
+                timer = 0;
+                introimage.enabled = false;
+                controlimage.enabled = true;
+
+            }
+
+        }
+
+        if (timer >= 215 && controlimage.enabled)
+        {
+            SceneManager.LoadScene("ExpandingSpace");
+        }
     }
 }
