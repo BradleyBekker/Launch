485f1e104aa988eff3161481ac6d0a31707690df
JudaHensen <24311@ma-web.nl>
2018-06-14 08:49:53 +0200

    re-organized scripts folder & Player gameobject

--- spacecake/Assets/Scripts/RocketParts/RocketPart3.cs
+++ spacecake/Assets/Scripts/RocketParts/RocketPart3.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class part4 : MonoBehaviour {
+public class RocketPart3 : MonoBehaviour {
     
     public AudioSource soundsource;
 
@@ -12,8 +12,7 @@ public class part4 : MonoBehaviour {
     [SerializeField] private Image Player1part;
     [SerializeField] private Image Player2part;
     Animator anim;
-    bool Colletable = true;
-
+    bool collectable = true;
     private void Start()
     {
         anim = GetComponent<Animator>();
@@ -27,44 +26,47 @@ public class part4 : MonoBehaviour {
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "player1" && P1rocket.GetComponent<P1rocket>().part4 == false && Colletable)
+        if (collision.gameObject.tag == "player1" && P1rocket.GetComponent<P1rocket>().part3 == false && collectable)
         {
             soundsource.Play();
-            Colletable = false;
-            anim.SetTrigger("pickup");
+            collectable = false;
             print("p1 got 3");
-            P1rocket.GetComponent<P1rocket>().part4 = true;
+            anim.SetTrigger("pickup");
+
+            P1rocket.GetComponent<P1rocket>().part3 = true;
             StartCoroutine(Wait());
             Player1part.enabled = true;
         }
-        if (collision.gameObject.tag == "player2" && P2rocket.GetComponent<P2rocket>().part4 == false && Colletable)
+        if (collision.gameObject.tag == "player2" && P2rocket.GetComponent<P2rocket>().part3 == false && collectable)
         {
             soundsource.Play();
-            Colletable = false;
-            anim.SetTrigger("pickup");
+            collectable = false;
             print("p2 got 3");
-            P2rocket.GetComponent<P2rocket>().part4 = true;
-            StartCoroutine(Wait());
+            anim.SetTrigger("pickup");
+
+            P2rocket.GetComponent<P2rocket>().part3 = true;
             Player2part.enabled = true;
+            StartCoroutine(Wait());
+
         }
 
     }
     void Imagechecks()
     {
-        if (P1rocket.GetComponent<P1rocket>().part4 == true)
+        if (P1rocket.GetComponent<P1rocket>().part3 == true)
         {
             Player1part.enabled = true;
         }
-        if (P1rocket.GetComponent<P1rocket>().part4 == false)
+        if (P1rocket.GetComponent<P1rocket>().part3 == false)
         {
             Player1part.enabled = false;
         }
 
-        if (P2rocket.GetComponent<P2rocket>().part4 == true)
+        if (P2rocket.GetComponent<P2rocket>().part3 == true)
         {
             Player2part.enabled = true;
         }
-        if (P2rocket.GetComponent<P2rocket>().part4 == false)
+        if (P2rocket.GetComponent<P2rocket>().part3 == false)
         {
             Player2part.enabled = false;
         }
@@ -78,4 +80,3 @@ public class part4 : MonoBehaviour {
 
     }
 }
-
